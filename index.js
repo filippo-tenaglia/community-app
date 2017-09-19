f57bf076a0fbb6ebce67e3293b89c6e667606463
Filippo Tenaglia <filippo.tenaglia@aesystech.it>
2017-09-19 14:46:20 +0200

    Fix ipaddress openshift request

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const server = require('http').createServer(app);
 const fs = require('fs');
 var port=8080;
-
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
 app.use(express.static(path.join(__dirname, 'public')));
 //console.log(path.join(__dirname,'public','index.html'));
@@ -12,7 +12,7 @@ app.get('/community/v1',function(req,res,next){
     res.sendFile(path.join(__dirname,'public','index.html'));
 });
 
-server.listen(port, function () {
+server.listen(port, ipaddress, function () {
   console.log('Server listening at port %d', port);
 });
 
